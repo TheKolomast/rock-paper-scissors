08bf3825ad8b096a77801642458948dd75ec6763
TheKolomast <thekolomast@gmail.com>
2024-11-08 12:45:14 +0100

    Added event listeners for buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Logic
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -12,29 +14,21 @@ function getComputerChoice () {
    return 'scissors';
 }
 
-function getPlayerChoice () {
-  let playerChoice = prompt('Choose rock, paper or scissors. Play 5 rounds and get to see if you can beat THE COMPUTER').toLowerCase();
-  if(playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
-    alert('You must make a valid choice');
-    return getPlayerChoice();
-  }
-  return playerChoice;
-}
 
-
-function playRound(computer, player) {
-  if(computer === player) {
-    alert(`Computer chose ${computer} and player chose ${player}. It's a tie!`);
+function playRound(player) {
+  const computerChoice = getComputerChoice();
+  if(computerChoice === player) {
+    alert(`Computer chose ${computerChoice} and player chose ${player}. It's a tie!`);
     return 'tie';
   } else if (
-    (computer === "scissors" && player === "rock") ||
-    (computer === "rock" && player === "paper") ||
-    (computer === "paper" && player === "scissors")
+    (computerChoice === "scissors" && player === "rock") ||
+    (computerChoice === "rock" && player === "paper") ||
+    (computerChoice === "paper" && player === "scissors")
   ) {
-    alert(`Computer chose ${computer} and player chose ${player}. Player wins round.`)    
+    alert(`Computer chose ${computerChoice} and player chose ${player}. Player wins round.`)    
     return playerScore++;
   } else {
-    alert(`Computer chose ${computer} and player chose ${player}. Computer wins round.`)
+    alert(`Computer chose ${computerChoice} and player chose ${player}. Computer wins round.`)
     return computerScore++;
   }   
 }
@@ -47,4 +41,12 @@ function playGame () {
   alert(`Final Score:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
 }
 
-playGame();
+// UI part 
+
+const rockButton = document.getElementById("rockBtn");
+const paperButton = document.getElementById("paperBtn");
+const scissorsButton = document.getElementById("scissorsBtn");
+
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
